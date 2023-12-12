5ba8c6a1c75da3853e604590571d89a6abb8254a
Griffin Stofko <gpstofko@cps.edu>
2023-12-12 11:31:41 -0600

    FINAL 12/12

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -4,51 +4,35 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 data = pd.read_csv("part4-classification/suv_data.csv")
-data['Gender'].replace(['Male','Female'],[0,1],inplace=True)
+data['Gender'].replace(['Male', 'Female'], [0, 1], inplace=True)
 
 x = data[["Age", "EstimatedSalary", "Gender"]].values
 y = data["Purchased"].values
 
 # Step 1: Print the values for x and y
-print(x)
-print(y)
+print("Values of x:")
+print(x)  
+print("\nValues of y:")
+print(y)  
 # Step 2: Standardize the data using StandardScaler, 
-scaler = StandardScaler().fit(x)
-
+#scaler = StandardScaler()
 # Step 3: Transform the data
-x = scaler.transform(x)
-
+#x_scaled = scaler.fit_transform(x)
 # Step 4: Split the data into training and testing data
-x_train, x_test, y_train, y_test = train_test_split(x, y)
-
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 # Step 5: Fit the data
-
 # Step 6: Create a LogsiticRegression object and fit the data
-model = linear_model.LogisticRegression().fit(x_train, y_train)
-
+logistic_model = linear_model.LogisticRegression()
+logistic_model.fit(x_train, y_train)
 # Step 7: Print the score to see the accuracy of the model
-print("Accuracy:", model.score(x_test, y_test))
-print("*************")
-print("Testing Results:")
-print("")
-
+accuracy = logistic_model.score(x_test, y_test)
+print("\nAccuracy of the model:", accuracy)
 # Step 8: Print out the actual ytest values and predicted y values
 # based on the xtest data
-print(y_test)
-for index in range(len(x_test)):
-    x = x_test[index]
-    x = x.reshape(-1, 3)
-    y_pred = int(model.predict(x))
+y_pred = logistic_model.predict(x_test)
 
-    if y_pred == 0:
-        y_pred = "Male"
-    elif y_pred == 1:
-        y_pred = "Female"
-    
-    actual = y_test[index]
-    if actual == 0:
-        actual = "Male"
-    elif actual == 1:
-        actual = "Female"
-    print("Predicted Gender: " + y_pred + " Actual Gender: " + actual)
-    print("")
+print("\nActual ytest values:")
+print(y_test)  
+
+print("\nPredicted y values:")
+print(y_pred)
--- part4-classification/a6_part4_writeup.md
+++ part4-classification/a6_part4_writeup.md
@@ -5,10 +5,14 @@ After completing `a6_part4.py` answer the following questions
 ## Questions to answer
 
 1. Comment out the StandardScaler and re-run your test. How accurate is the model? Why is that?
+Not very accurate, as many of the predicted genders compared to the actual genders are wrong. Standard scalar centers the values around the mean and allows for more accurate predictions. Also the accurarcy of the model without the standard scaler was 0.6875 which is quite low and not above 75% which is needed to justify correlation.
 
 2. How accurate is the model with the StandardScaler? Is this model accurate enough for the given use case? Explain.
+The model is much more accurate with the standard scaler, 
+With the standard scaler the accuracy is 0.8625, while without the standard scaler the accuracy of the model was 0.6875. The model is accurate enough with the standard scaler as 0.8625 is close to 1 so its mostly accurate.
 
 3. Looking at the predicted and actual results, how did the model do? Was there a pattern to the inputs that the model was incorrect about?
+There was no pattern I could see, and the model did pretty well as especially with the standard scaler with 86% accurary.
 
 4. Would a 34 year old Female who makes 56000 a year buy an SUV according to the model? Remember to scale the data before running it through the model.
-
+No they would not.
